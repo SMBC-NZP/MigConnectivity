9e688bb17d8348bc62eb9d84d3262b18e696b49a
falafelbot <hostetlerj@si.edu>
2017-02-17 17:13:36 -0500

    Added calcMCSmall to calculate exact MC for small abundances per population

--- R/calcConnectivity.R
+++ R/calcConnectivity.R
@@ -62,6 +62,81 @@ calcMC <- function(originDist, targetDist, psi, originRelAbund) {
 }
 
 ###############################################################################
+# Calculate MC for known actual abundance per site
+###############################################################################
+#' Migratory connectivity strength function corrected for small absolute
+#' abundances
+#'
+#' @param originDist Distances between the B origin sites.  Symmetric B by B
+#'    matrix.
+#' @param targetDist Distances between the W target sites.  Symmetric W by W
+#'    matrix.
+#' @param psi Transition probabilities between B origin and W target sites.
+#'    Matrix with B rows and W columns where rows sum to 1.
+#' @param originAbund Actual or estimated abundances at B origin sites.
+#'    Numeric vector of length B with all values greater than or equal to 1.
+#'    These should be on the scale of number of individuals.
+#'
+#' @return real value between -1 and 1, inclusive (usually between 0 and 1)
+#'    indicating the degree of migratory connectivity.
+#'
+#' @seealso calcMC
+#' @export
+calcMCSmall <- function(originDist, targetDist, psi, originAbund) {
+  nOrigin <- nrow(psi)
+  nTarget <- ncol(psi)
+  # Check inputs
+  if (nrow(originDist)!=ncol(originDist) || !isSymmetric(unname(originDist)))
+    stop('originDist (distances between origin sites) must be a square, symmetric matrix.')
+  if (nrow(targetDist)!=ncol(targetDist) || !isSymmetric(unname(targetDist)))
+    stop('targetDist (distances between target sites) must be a square, symmetric matrix.')
+  if (ncol(originDist)!=nOrigin || ncol(targetDist)!=nTarget ||
+      !isTRUE(all.equal(rowSums(psi), rep(1, nOrigin), tolerance = 1e-6,
+                        check.attributes = F))) {
+    if (ncol(targetDist)!=nOrigin || ncol(originDist)!=nTarget ||
+        !isTRUE(all.equal(colSums(psi), rep(1, nTarget), tolerance = 1e-6,
+                          check.attributes = F)))
+      stop('psi should have [number of origin sites] rows and [number of target sites] columns and rows that sum to 1.')
+    else { #Just turn it around
+      psi <- t(psi)
+      nOrigin <- nrow(psi)
+      nTarget <- ncol(psi)
+    }
+  }
+  if (length(originAbund)!=nOrigin || any(originAbund < 1))
+    stop('originAbund must be a vector with [number of origin sites] values >= 1.')
+  originRelAbund <- originAbund / sum(originAbund)
+  originRelAbund2 <- (matrix(rep(originAbund, each = nOrigin), nOrigin, nOrigin) -
+                        diag(nrow = nOrigin)) *
+    matrix(rep(originRelAbund, nOrigin), nOrigin, nOrigin) / (sum(originAbund) - 1)
+  # Relative abundance at each combination of origin and target sites
+  sumWinRelN <- apply(psi, 2, "*", originRelAbund)
+  sumWinN <- apply(psi, 2, "*", originAbund)
+  subMat <- matrix(0, nOrigin ^ 2, nTarget ^ 2)
+  subMat[rep(1:nOrigin, nOrigin) == rep(1:nOrigin, each = nOrigin),
+         rep(1:nTarget, nTarget) == rep(1:nTarget, each = nTarget)] <- 1
+  sumWinRelN2 <- (sumWinRelN %x% matrix(1, nOrigin, nTarget)) *
+    ((matrix(1, nOrigin, nTarget) %x% sumWinN) - subMat) / (sum(originAbund) - 1) #rep(sumWinRelN, each = nOrigin), nOrigin^2, nTarget^2)
+  # Average origin distance between any two given birds.  Using matrix product (%*%) to get all probabilities of two birds origin in each site.
+  mu.bD <- sum(originDist * originRelAbund2)
+  # SD in origin distance between any two given birds
+  sd.bD <- sqrt(sum((originDist - mu.bD)^2 * originRelAbund2))
+  # Average target distance between any two given birds.  Using Kronecker product (%x%) to get all probabilities of two birds origin and target in each pair of sites.
+  mu.wD <- sum(targetDist * matrix(colSums(sumWinRelN2), nTarget, nTarget))
+  # SD in target distance between any two given birds
+  sd.wD <- sqrt(sum((targetDist - mu.wD)^2 * matrix(colSums(sumWinRelN2), nTarget, nTarget)))
+  # Correlation between origin and target distances, calculated from formula:
+  # sum(probability of each combination of origin sites and target sites *
+  # (distance between origin sites - mean origin site distance) * (distance between target sites - mean target site distance)) /
+  # (SD in origin site distances * SD in target site distances)
+  MC <- sum(sumWinRelN2 *
+              (matrix(rep(originDist, nTarget^2), nOrigin^2, nTarget^2) - mu.bD) *
+              (matrix(rep(targetDist, nOrigin^2), nOrigin^2, nTarget^2, byrow=T) - mu.wD))/(sd.bD*sd.wD)
+  return(MC)
+}
+
+
+###############################################################################
 # rho function for individuals
 ###############################################################################
 calcStrengthInd <- function(originDist, targetDist, locations, resamp=1000, verbose = 0) {
