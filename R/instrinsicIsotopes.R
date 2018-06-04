dd4a5d6e7e3b225f4f38769bbcf2ba56496a5773
mhallwor <hallworthm@si.edu>
2018-06-04 15:58:21 -0400

    added class & streamlined code. Fixed error in assignment & now returns all outputs

--- R/instrinsicIsotopes.R
+++ R/instrinsicIsotopes.R
@@ -9,10 +9,7 @@
 #' @param isoSTD standard deviation from calibration
 #' @param intercept intercept value from calibration
 #' @param slope value from calibration
-#' @param oddsRatio logical to convert probability into odds ratio
 #' @param odds if \code{oddsRatio} the odds ratio to use to set likely and unlikely locations
-#' @param singleCellAssign if TRUE generates \code{nSamples} single location assignments using a multinomial
-#'        distribution using the assignment probability.
 #' @param restrict2Likely if \code{TRUE} restricts locations to fall within the most probable assignment
 #'        locations if \code{singleCellAssign = TRUE}. Only used when \code{oddsRatio = TRUE}.
 #' @param nSamples integer specifying how many random samples to draw from a multinomial distribution.
@@ -22,9 +19,6 @@
 #'        areas.
 #' @param assignExtent definition for the extent of the assignment. Can be used in place of \code{sppShapefile} to
 #'        limit assignment. Input should follow \code{c(xmin,xmax,ymin,ymax)} in degrees longitude and latitude.
-#' @param return which assignment map to return. 'probability' returns assignment probability.
-#'     'odds' returns a single map for each isovalue as likely or unlikely assignment. 'population'
-#'     the population level assignment (sum of all 'odds').
 #' @param element The elemental isotope of interest. Currently the only
 #'     elements that are implemented are 'Hydrogen' (default) and 'Oxygen'
 #' @param surface if "TRUE" returns surface water values. Defaults is 'FALSE'
@@ -34,7 +28,14 @@
 #'     'GrowingSeason' returns growing season values in precipitation for
 #'      \code{element} of interest.
 #'
-#' @return raster stack, raster layer or data.frame if \code{dataFrame = TRUE} of assignment probabilites
+#' @return returns an \code{isoAssign} object containing the following:
+#'     1. raster stack of individual probabilistic assignments,
+#'     2. raster stack that includes likely vs unlikely origin for each animal,
+#'     3. a raster for population level assignment (sum of # 2),
+#'     4. data.frame of individual probability surfaces,
+#'     5. data.frame of likely vs unlikley surfaces,
+#'     6. data.frame of population level assignment &
+#'     7. array of coordinates (longitude,latitude) for single cell assignment
 #'
 #' @export
 #'
@@ -46,66 +47,58 @@
 #'
 #' OVENvals <- read.csv("data-raw/deltaDvalues.csv")
 #'
-#' a <- Sys.time()
-#' b <- isoAssign(isovalues = OVENvals[,2],
+#'a <- Sys.time()
+#'b <- isoAssign(isovalues = OVENvals[,2],
 #'               isoSTD = 12,
 #'               intercept = -10,
 #'               slope = 0.8,
-#'               oddsRatio = FALSE,
 #'               odds = NULL,
-#'               SingleCellAssign = TRUE,
 #'               restrict2Likely = TRUE,
 #'               nSamples = 1000,
-#'               dataFrame = FALSE,
 #'               sppShapefile = OVENdist,
 #'               assignExtent = c(-179,-60,15,89),
-#'               return = "sim.cell",
 #'               element = "Hydrogen",
 #'               surface = FALSE,
 #'               period = "Annual")
-#' Sys.time()-a}
+#'Sys.time()-a
+#'}
 
 isoAssign <- function(isovalues,
                       isoSTD,
                       intercept,
                       slope,
-                      oddsRatio = FALSE,
-                      odds = NULL,
-                      SingleCellAssign = FALSE,
+                      odds = 0.33,
                       restrict2Likely = TRUE,
                       nSamples = NULL,
-                      dataFrame = FALSE,
                       sppShapefile = NULL,
                       assignExtent = c(-179,-60,15,89),
-                      return = "probability",
                       element = "Hydrogen",
                       surface = FALSE,
                       period = "Annual"){
-
-  # quick input check #
-if(!(return %in% c("probability","population","odds","sim.cell"))){
-  stop("return must be either probability, population, odds, or sim.cell")}
-
 # download isoscape map
 isomap <- getIsoMap(element = element, surface = surface, period = period)
 
-# Series of checks for a species range map inputs
 # 1. if sppShapefile == NULL - use extent option
 if(is.null(sppShapefile)){
   isomap <- raster::crop(isomap,raster::extent(assignExtent))
 }
+
+# Series of checks for a species range map inputs
+if(!is.null(sppShapefile)){
 # 2. if sppShapefile provided check that it has a projection defined
 #    if not stop - if so, mask the isoscape to range
-if(is.na(raster::crs(sppShapefile))){
-  stop("coordinate system needed for sppShapefile")}
+  if(is.na(raster::crs(sppShapefile))){
+    stop("coordinate system needed for sppShapefile")}
 # 3. if the projections don't match - project into same as isomap then mask
-if(sppShapefile@proj4string@projargs != isomap@crs@projargs){
-  sppShapefile <- sp::spTransform(sppShapefile, sp::CRS(isomap@crs@projargs))
-}
+  if(sppShapefile@proj4string@projargs != isomap@crs@projargs){
+    sppShapefile <- sp::spTransform(sppShapefile, sp::CRS(isomap@crs@projargs))
+  }
 
-isomap <- raster::crop(isomap, sppShapefile)
+cat("\n Restricting possible assignments to species distribution \n")
+
+# mask the isomap to sppShapefile
 isomap <- raster::mask(isomap, sppShapefile)
-
+}
 
 # generate a 'feather'/animal isoscape
 animap <- raster::calc(isomap, function(x){y <- slope*x+intercept})
@@ -113,76 +106,98 @@ animap <- raster::calc(isomap, function(x){y <- slope*x+intercept})
 # spatially explicit assignment
 assign <- function(x,y) {((1/(sqrt(2 * 3.14 * isoSTD))) * exp((-1/(2 * isoSTD^2)) * ((x) - y)^2))}
 
+# apply the assignment function to all input values
+cat("\n Generating probabilistic assignments \n")
+
 assignments <- lapply(isovalues, FUN = function(x){assign(x, y = animap)})
 
+# stack the assignment probabilities into a single raster stack
 assignments <- raster::stack(assignments)
 
 # Transform the assignments into a true probability surface #
 assign2prob <- assignments/raster::cellStats(assignments, sum)
 
-if(dataFrame == TRUE) {
-  assign2probDF <- data.frame(raster::rasterToPoints(assign2prob))
-}
+# Create a dataframe with XY coords and probabilites for each animal
+assign2probDF <- data.frame(raster::rasterToPoints(assign2prob))
 
+# function to make an odds ratio (likely vs unlikely) assignment
 oddsFun <- function(x,odds = odds){
   predict(smooth.spline(x = cumsum(sort(x)),
                         sort(x),
                         spar = 0.1),odds)$y
 }
 
-if(oddsRatio == TRUE) {
- if (is.null(odds))
-   odds <- 0.33
+# if odds is left null - use default of 0.33
+ if (is.null(odds)){odds <- 0.33}
 
-  matvals <- raster::values(assign2prob)
+# extract values from the probability assignment
+matvals <- raster::values(assign2prob)
 
-  cuts <- apply(matvals,2,FUN = oddsFun,odds = odds)
+cat("\n Generating likely vs unlikely assignments \n")
+# apply the odds function
+cuts <- apply(matvals,2,FUN = oddsFun,odds = odds)
 
-  step1 <- raster::reclassify(assign2prob,cbind(0,cuts,0))
-  step2 <- raster::reclassify(step1,cbind(cuts,1,1))
+# reclassify the rasters based on likely v unlikely
+step1 <- mapply(FUN=function(x,y){raster::reclassify(assign2prob[[x]],cbind(0,y,0))},
+                x = 1:raster::nlayers(assign2prob),
+                y=cuts)
+step1 <- raster::stack(step1)
+step2 <- mapply(FUN=function(x,y){raster::reclassify(step1[[x]],cbind(y,1,1))},
+                x = 1:raster::nlayers(step1),
+                y=cuts)
+step2 <- raster::stack(step2)
+#step1 <- raster::reclassify(assign2prob,cbind(0,cuts,0))
+#step2 <- raster::reclassify(step1,cbind(cuts,1,1))
 
-  SamplePop <- sum(step2)
+# convert to dataframe
+LikelyUnlikely <- raster::rasterToPoints(step2)
+step2DF <- data.frame(LikelyUnlikely)
 
-  if(dataFrame == TRUE) {
-    step2DF <- data.frame(raster::rasterToPoints(step2))
-    SamplePopDF <- data.frame(raster::rasterToPoints(SamplePop))
-  }
-}
+# Return the population level odds assignment - i.e, how many animals
+SamplePop <- sum(step2)
+# convert to dataframe
+SamplePopDF <- data.frame(raster::rasterToPoints(SamplePop))
 
-if (SingleCellAssign == TRUE) {
-  if (is.null(nSamples))
-    nSamples <- 1000
-  xysim <- array(NA, c(nSamples, 2, raster::nlayers(assign2prob)))
+# SINGLE CELL PROBABILITY ASSIGNMENTS - this makes MC possible with isotopes
+if (is.null(nSamples)){ nSamples <- 1000}
+
+
+# generate empty array to fill with locations
+xysim <- array(NA, c(nSamples, 2, raster::nlayers(assign2prob)))
+# name the array
   #dimnames(xysim)[[1]] <- 1:nSamples
   dimnames(xysim)[[2]] <- c("Longitude","Latitude")
   dimnames(xysim)[[3]] <- names(assign2prob)
 
-  # converts raster to matrix of XY then probs
-  matvals <- raster::rasterToPoints(assign2prob)
+# converts raster to matrix of XY then probs
+matvals <- raster::rasterToPoints(assign2prob)
 
-  # Restrict random point estimates to 'likely' origin area #
-  if(restrict2Likely && oddsRatio){
-    binaryLikely <- raster::rasterToPoints(step2)
-    matvals[,3:ncol(matvals)] <- matvals[,3:ncol(matvals)] * binaryLikely[,3:ncol(matvals)]
-    matvals[matvals==0] <- NA
-  }
+# Restrict random point estimates to 'likely' origin area #
+if(restrict2Likely){
+    matvals[,3:ncol(matvals)] <- matvals[,3:ncol(matvals)] * LikelyUnlikely[,3:ncol(matvals)]
+}
 
+cat("\n Generating single cell assignments \n")
   # This draws samples nSamples per animal (faster than looping over nSamples) and fills the xysim with x,y coords
-  for(i in 3:ncol(matvals)) {
+for(i in 3:ncol(matvals)) {
     multidraw <- rmultinom(n = nSamples, size = 1, prob = matvals[,i])
     xysim[,1,i-2] <- matvals[which(multidraw == 1, arr.ind = TRUE)[,1],1]
     xysim[,2,i-2] <- matvals[which(multidraw == 1, arr.ind = TRUE)[,1],2]
-  }
 }
 
-#What should be returned
-if(return == "probability" & dataFrame == FALSE){return(assign2prob)}
-if(return == "probability" & dataFrame == TRUE){return(assign2probDF)}
-if(return == "odds" & dataFrame == FALSE){return(step2)}
-if(return == "odds" & dataFrame == TRUE){return(step2DF)}
-if(return == "population" & dataFrame == FALSE){return(SamplePop)}
-if(return == "population" & dataFrame == TRUE){return(SamplePopDF)}
-if(return == "sim.cell"){return(xysim)}
+
+isoAssignReturn <- structure(list(probassign = assign2prob,
+                        oddsassign = step2,
+                        popassign = SamplePop,
+                        probDF = assign2probDF,
+                        oddsDF = step2DF,
+                        popDF = SamplePopDF,
+                        SingleCell = xysim),
+                        class = "isoAssign")
+
+
+cat("\n Done \n")
+return(isoAssignReturn)
 }
 
 
