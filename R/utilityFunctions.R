3d4c6d29ab4c036fee7d46049aab56e621ee0f1a
falafelbot <hostetlerj@si.edu>
2021-04-23 10:23:31 -0400

    Removed old targetSample function; continued edits to locSample for rasters

--- R/utilityFunctions.R
+++ R/utilityFunctions.R
@@ -44,86 +44,6 @@ mlogitMC <- function(slope, MC.in, origin.dist, target.dist, origin.rel.abund) {
   return((MC.in - MC)^2)
 }
 
-# Samples from GL and/or GPS locations
-# Called by estMantel and estMCGlGps
-targetSample <- function(isGL, geoBias, geoVCov, targetPoints, animal.sample,
-                         nSim = 1000, targetSites = NULL, targetAssignment = NULL,
-                         resampleProjection = "+proj=eqc +lat_ts=0 +lat_0=0 +lon_0=0 +x_0=0 +y_0=0 +a=6371007 +b=6371007 +units=m +no_defs",
-                         maxTries = 300) {
-  nAnimals <- length(targetPoints)
-
-  # targetDist1 <- matrix(NA, nAnimals, nAnimals)
-  #
-  # targetDist1[lower.tri(targetDist1)] <- 1
-  #
-  # distIndices <- which(!is.na(targetDist1), arr.ind = T)
-  #
-  # # project target points to WGS #
-  # targetPoints2 <- sp::spTransform(targetPoints, sp::CRS(WGS84))
-  #
-  # targetDist0 <- geosphere::distVincentyEllipsoid(targetPoints2[distIndices[,'row'],],
-  #                                                 targetPoints2[distIndices[,'col'],])
-  #
-  # targetDist1[lower.tri(targetDist1)] <- targetDist0
-
-  if (is.null(targetAssignment)) {
-    if (!is.null(targetSites))
-      targetAssignment <- sp::over(targetPoints, y = targetSites)
-    else
-      targetAssignment <- rep(0, nAnimals)
-  }
-
-  # Storage
-  target.sample <- rep(NA, nAnimals)
-  target.point.sample <- matrix(NA, nAnimals, 2)
-  # Fill in GPS values (no location uncertainty)
-  target.sample[which(!isGL[animal.sample])] <- targetAssignment[animal.sample[which(!isGL[animal.sample])]]
-  target.point.sample[which(!isGL[animal.sample]), ] <- targetPoints[animal.sample[which(!isGL[animal.sample])],]@coords
-  # Which to sample still
-  toSample <- which(isGL[animal.sample])
-  # In this case, only have to sample each GL animal once, because no restrictions
-  if (is.null(targetSites) && any(isGL[animal.sample])) {
-    draws <- 1
-    geoBias2 <- array(rep(geoBias, length(toSample)), c(2, length(toSample)))
-    point.sample <- array(apply(targetPoints@coords[animal.sample[toSample], , drop = FALSE], 1,
-                                MASS::mvrnorm, n=1, Sigma=geoVCov),
-                          c(2, length(toSample))) - geoBias2
-    point.sample <- sp::SpatialPoints(point.sample, proj4string = sp::CRS(resampleProjection))
-    target.point.sample[toSample, ]<- t(point.sample@coords)
-  }
-  else {
-    draws <- 0
-    while (length(toSample) > 0 && (is.null(maxTries) || draws <= maxTries)) {
-      draws <- draws + 1
-      # Create giant version of geoBias we can subtract from point.sample
-      geoBias2 <- array(rep(geoBias, length(toSample), each = nSim), c(nSim, 2, length(toSample)))
-      # Draw random points for GL animals from multivariate normal distribution
-      point.sample <- array(apply(targetPoints@coords[animal.sample[toSample], , drop = FALSE], 1,
-                                  MASS::mvrnorm, n=nSim, Sigma=geoVCov),
-                            c(nSim, 2, length(toSample))) - geoBias2
-      # Convert those to SpatialPoints
-      point.sample <- apply(point.sample, 3, sp::SpatialPoints,
-                            proj4string = sp::CRS(resampleProjection))
-      # Find out which sampled points are in a target site
-      target.sample0 <- sapply(point.sample, sp::over, y = targetSites)
-      # Identify which animals have at least one valid sample point. good.sample
-      # will be NA for those that don't.  For those that do, it will location in
-      # point.sample first valid point can be found.
-      good.sample <- apply(target.sample0, 2, function(x) which(!is.na(x))[1])
-      # Fill in target site of valid sampled points
-      target.sample[toSample] <- apply(target.sample0, 2, function(x) x[!is.na(x)][1])
-      # Fill in target points of valid sampled points
-      if (any(!is.na(good.sample)))
-        target.point.sample[toSample[!is.na(good.sample)], ]<- t(mapply(function(x, y) y[x]@coords,
-                                                                        good.sample[!is.na(good.sample)], point.sample[!is.na(good.sample)]))
-      toSample <- which(is.na(target.sample))
-    }
-    if (!is.null(maxTries) && draws > maxTries)
-      stop(paste0('maxTries (',maxTries,') reached during point resampling, exiting. Examine targetSites, geoBias, and geoVcov to determine why so few resampled points fall within targetSites.'))
-  }
-  return(list(target.sample = target.sample, target.point.sample = target.point.sample,
-              draws = draws))
-}
 
 # Samples from GL and/or GPS locations
 # Called by estMantel and estMCGlGps
@@ -439,6 +359,21 @@ locSample <- function(isGL,
           t(mapply(x = good.sample0[!is.na(good.sample0) & !(which(isGL & toSampleBool) %in% which(isProb | isRaster))],
                    y = point.sample0[which(!(which(isGL & toSampleBool) %in% which(isProb | isRaster)) & (!is.na(good.sample0)))],
                    FUN = function(x, y) sf::st_coordinates(y[x,])))
+      }
+    }
+    if (any(!isProb & !isGL & isRaster & toSampleBool)){
+       if (any(!is.na(good.sample2[!(which(isRaster) %in% which(isProb | isGL))]))){
+          site.sample[which(!isProb & !isGL & isRaster & toSampleBool)] <-
+            apply(site.sample2[, !(which(isRaster & toSampleBool) %in% which(isProb | isGL)),
+                               drop = FALSE],
+                  2,
+                  function(x) x[!is.na(x)][1])
+          # Fill in target points of valid sampled points
+          point.sample[which(!isProb & !isGL & isRaster & toSampleBool)[which(!is.na(good.sample2))], ]<-
+            t(mapply(x = good.sample12[!is.na(good.sample12)],
+                     y = point.sample2[which(which(isRaster & toSampleBool) %in% which(!isProb & !isGL & !is.na(good.sample2)))],
+                     FUN = function(x, y) sf::st_coordinates(y[x,])))
+        }
       }
     }
     if (any(isProb & !isGL & isRaster & toSampleBool)){
