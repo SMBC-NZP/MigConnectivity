0866f4bd97cdef0f26a98dca637cbad381166e9d
MTHallworth <mhallworth@gmail.com>
2021-08-30 09:23:17 -0400

    added crs to simulated rasters

--- R/simConnectivity.R
+++ R/simConnectivity.R
@@ -698,11 +698,13 @@ simGeneticData <- function(genPops,
                      function(x){
                       raster::calc(x*genPops$popRast / Ncell, fun = sum)
                      }))
-  if (captured=="target")
-    crs(indRasts) <- sf::st_crs(targetSites)
-  else
-    crs(indRasts) <- sf::st_crs(originSites)
-
+  if (captured=="target"){
+    tsCRS <- sf::st_crs(targetSites, parameters = TRUE)
+    crs(indRasts) <- sp::CRS(tsCRS$proj4string) # sf::st_crs(targetSites)
+  }else{
+    osCRS <- sf::st_crs(originSites, parameters = TRUE)
+    crs(indRasts) <- sp::CRS(osCRS$proj4string) #sf::st_crs(originSites)
+    }
   return(list(originAssignment = originAssignment,
               targetAssignment = targetAssignment,
               originPointsTrue = originPointsTrue,
